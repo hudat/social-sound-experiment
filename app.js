c49bcf4422c23d035066424f59598121a8e3ee14
Husam Machlovi <husam@withpulp.com>
2015-08-14 00:40:58 -0400

    searched tweets for emotion and stored chord type in array based on emotion

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("yo world");
+var getTweet1 = function() {
+  return "Feeling kinda down today. My favorite team lost"
+}
+
+var getTweet2 = function() {
+  return "Excited for this new role!"
+}
+
+tweet1 = getTweet1();
+
+emotion = tweet1.search("down");
+
+if (emotion == -1) {
+  console.log("down was not found in that tweet")
+};
+
+if (emotion != -1) {
+  var chords = []
+  chords.push("major");
+}
+
+console.log(chords);
