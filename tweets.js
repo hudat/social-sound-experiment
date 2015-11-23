9f8167d3441d90493da4176fad262a8476e8d647
Husam Machlovi <husam@withpulp.com>
2015-11-22 23:04:32 -0500

    tweets mock data, grabbed length of object

---
+++ tweets.js
@@ -0,0 +1,40 @@
+{
+  "tweets": [
+    {
+      "tweet": "scared to leave my house. the world's a mess",
+      "user": "blondesally"
+    },
+    {
+      "tweet": "excited for this new project",
+      "user": "dwaynejohnson"
+    },
+    {
+      "tweet": "pissed off at this fucking mediocre scumbags",
+      "user": "billhicks"
+    },
+    {
+      "tweet": "happy happenstance",
+      "user": "harleyquinn"
+    },
+    {
+      "tweet": "using sadness to guide",
+      "user": "joker"
+    },
+    {
+      "tweet": "surprised to see hyde",
+      "user": "jekyll"
+    },
+    {
+      "tweet": "rage is here",
+      "user": "morello"
+    },
+    {
+      "tweet": "confused here at the crossroads",
+      "user": "faulkner"
+    },
+    {
+      "tweet": "free and alive",
+      "user": "robertjohnson"
+    }
+  ]
+}
